c2086c704e82fe4173cc9d271f7b6befa029e43c
Shu <92517496+HUSTShu@users.noreply.github.com>
2022-12-08 20:46:49 +0700

    Add files via upload

---
+++ packages/v0.7.6/index.ts
@@ -0,0 +1,14 @@
+import { compile } from "solc";
+
+import { IInput } from "../../src/Interfaces/input.interface";
+
+export const v0_7_6 = (filename: string, input: IInput) => {
+    const output = JSON.parse(compile(JSON.stringify(input)));
+    const contracts = output.contracts[filename];
+    const bytecode = Object.keys(contracts).reduce(
+        (pre, key) => pre + contracts[key].evm.bytecode.object,
+        ""
+    );
+
+    return bytecode;
+};
