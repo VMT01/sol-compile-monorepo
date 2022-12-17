9638761690578800b6c28b0c016cd2e58e985bdd
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2022-12-17 10:21:53 +0700

    feat: remove Other folder to compile

--- src/compile.ts
+++ src/compile.ts
@@ -29,21 +29,25 @@ function setup(): void {
     ensureDirSync(errorPath);
 }
 
-function main(dir: string, level: string[] = []): void {
-    readdirSync(dir).forEach((item) => {
-        const itemPath = resolve(dir, item);
-        if (lstatSync(itemPath).isDirectory()) {
-            console.log(`Start compile in: ${itemPath}`);
-            main(itemPath, [...level, item]);
-        } else {
+function main(dir: string): void {
+    const vulnerabilities: string[] = readdirSync(dir);
+    for (const vulnerability of vulnerabilities) {
+        if (vulnerability === "Other") continue;
+
+        const vulnerabilityPath: string = resolve(dir, vulnerability);
+        readdirSync(vulnerabilityPath).forEach((item) => {
             const filename = item.replace(/\.[^/.]+$/, "");
-            const source: string = readFileSync(itemPath, "utf8");
+            const filepath = resolve(vulnerabilityPath, item);
+            const source: string = readFileSync(filepath, "utf8");
 
             try {
                 const { avaiable, version } = detectVersion(source);
                 if (!avaiable) throw new Error();
 
-                const outPath: string = createOutputPath(level, filename);
+                const outPath: string = createOutputPath(
+                    [vulnerability],
+                    filename
+                );
                 const input: IInput = createInput(filename, source, version);
                 const output: string | null = compile(filename, version, input);
 
@@ -51,11 +55,40 @@ function main(dir: string, level: string[] = []): void {
             } catch (err: any) {
                 console.log(err.message);
 
-                const errPath: string = createErrorPath(level, filename);
+                const errPath: string = createErrorPath(
+                    [vulnerability],
+                    filename
+                );
                 outputFileSync(errPath, source);
             }
-        }
-    });
+        });
+    }
+    // readdirSync(dir).forEach((item) => {
+    //     const itemPath = resolve(dir, item);
+    //     if (lstatSync(itemPath).isDirectory()) {
+    //         console.log(`Start compile in: ${itemPath}`);
+    //         main(itemPath, [...level, item]);
+    //     } else {
+    //         const filename = item.replace(/\.[^/.]+$/, "");
+    //         const source: string = readFileSync(itemPath, "utf8");
+
+    //         try {
+    //             const { avaiable, version } = detectVersion(source);
+    //             if (!avaiable) throw new Error();
+
+    //             const outPath: string = createOutputPath(level, filename);
+    //             const input: IInput = createInput(filename, source, version);
+    //             const output: string | null = compile(filename, version, input);
+
+    //             if (output !== null) outputFileSync(outPath, output);
+    //         } catch (err: any) {
+    //             console.log(err.message);
+
+    //             const errPath: string = createErrorPath(level, filename);
+    //             outputFileSync(errPath, source);
+    //         }
+    //     }
+    // });
 }
 
 setup();
